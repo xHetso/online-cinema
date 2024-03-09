5757a26f4d1b3012ee2124c411518c70c222e2b4
Hetso <datacener23491248@gmail.com>
2024-03-09 15:25:32 +0500

    Add user-related modules and files

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: any) {
+    return this.authService.register(dto)
+  }
+}
