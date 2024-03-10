0de2ed8e49fde360e8625a340c0e001ead6aacd1
Hetso <datacener23491248@gmail.com>
2024-03-11 02:47:00 +0500

    Implementation of token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {
 	Injectable,
 	UnauthorizedException,
 } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { compare, genSalt, hash } from 'bcryptjs'
 import { InjectModel } from 'nestjs-typegoose'
@@ -13,10 +14,17 @@ import { AuthDto } from './dto/auth.dto'
 export class AuthService {
 	constructor(
 		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService,
 	) {}
 
 	async login(dto: AuthDto) {
-		return this.validateUser(dto)
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokenPair(String(user._id))
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
 	}
 
 	async register(dto: AuthDto) {
@@ -32,7 +40,12 @@ export class AuthService {
 			password: await hash(dto.password, salt),
 		})
 
-		return newUser.save()
+		const tokens = await this.issueTokenPair(String(newUser._id))
+
+		return {
+			user: this.returnUserFields(newUser),
+			...tokens,
+		}
 	}
 	async validateUser(dto: AuthDto): Promise<UserModel> {
 		const user = await this.UserModel.findOne({ email: dto.email })
@@ -45,5 +58,25 @@ export class AuthService {
 		if (!isValidPassword) throw new UnauthorizedException('Invalid password')
 
 		return user
+	}
+
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId }
+
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d',
+		})
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '1h',
+		})
+		return { refreshToken, accessToken }
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			_id: user._id,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		}
 	}
 }
