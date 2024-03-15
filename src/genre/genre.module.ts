df827be0d5d409c01ca2634606abf0fe35f17d12
Hetso <datacener23491248@gmail.com>
2024-03-15 11:40:28 +0500

    Add Genre module and related files

---
+++ src/genre/genre.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { GenreController } from './genre.controller'
+import { GenreModel } from './genre.model'
+import { GenreService } from './genre.service'
+
+@Module({
+	controllers: [GenreController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: GenreModel,
+				schemaOptions: {
+					collection: 'Genre',
+				},
+			},
+		]),
+	],
+	providers: [GenreService],
+})
+export class GenreModule {}
