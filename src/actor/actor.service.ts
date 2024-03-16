7e5f4d1e760f1bbec3b3f5476b8c33ad1c378805
Hetso <datacener23491248@gmail.com>
2024-03-16 20:14:29 +0500

    Refactor ActorService to use aggregation pipeline for improved performance

--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -32,12 +32,24 @@ export class ActorService {
 				],
 			}
 
-		// Aggregation
-
-		return this.ActorModel.find(options)
-			.select('-updatedAt -__v')
+		return this.ActorModel.aggregate()
+			.match(options)
+			.lookup({
+				from: 'Movie',
+				foreignField: 'actors',
+				localField: '_id',
+				as: 'movies',
+			})
+			.addFields({
+				countMovies: { $size: '$movies' },
+			})
+			.project({
+				__v: 0,
+				updatedAt: 0,
+				movies: 0,
+			})
 			.sort({
-				createdAt: 'desc',
+				createdAt: -1,
 			})
 			.exec()
 	}
